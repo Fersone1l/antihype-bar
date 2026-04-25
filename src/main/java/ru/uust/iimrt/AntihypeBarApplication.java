1389dfcb815b32fc22138b10f29ebee094f294e0
fersoneil <Fersoneil@yandex.ru>
2026-04-25 11:36:41 +0500

    updated pom and added packages

---
+++ src/main/java/ru/uust/iimrt/AntihypeBarApplication.java
@@ -0,0 +1,12 @@
+package ru.uust.iimrt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntihypeBarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AntihypeBarApplication.class, args);
+    }
+
+}
