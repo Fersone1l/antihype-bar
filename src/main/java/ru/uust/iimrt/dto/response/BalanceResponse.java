c16941d613dcfdaf348b0ed3fd7038f62f15ef71
fersoneil <Fersoneil@yandex.ru>
2026-04-26 12:54:31 +0500

    pipew

--- src/main/java/ru/uust/iimrt/dto/response/BalanceResponse.java
+++ src/main/java/ru/uust/iimrt/dto/response/BalanceResponse.java
@@ -1,10 +1,12 @@
 package ru.uust.iimrt.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({"status", "balance", "mood_level"})
 public class BalanceResponse {
     private String status = "ok";
     private int balance;
